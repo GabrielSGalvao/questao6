418fe5aa99f4158498ae9b9e3a5811885247f6b0
GabrielSGalvao <115426581+GabrielSGalvao@users.noreply.github.com>
2023-05-02 19:30:33 -0300

    Add files via upload

---
+++ src/Controlador/Fachada.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import DAO.*;
+import Bean.*;
+import Conexao.*;
+import java.util.*;
+import java.sql.SQLDataException;
+
+/**
+ *
+ * @author autologon
+ */
+public class Fachada {
+    
+}
